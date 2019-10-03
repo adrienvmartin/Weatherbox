7298b90aa5cfbc0dcc6f4e07965e25826f0e92af
Adrien Martin <adrien.v.martin@gmail.com>
2019-10-02 21:18:09 -0600

    Convert class to functional component

--- src/components/Grid/Grid_NoRedux.tsx
+++ src/components/Grid/Grid_NoRedux.tsx
@@ -1,39 +1,30 @@
 import React from "react";
 import ReactDataGrid from "react-data-grid";
 import { Paper, Button } from "@material-ui/core";
-import { columns } from "../../constants";
 
 interface IProps {
+  columns: object[];
   selectedRows: object[];
-  onGridRowsUpdated: ({fromRow, toRow, updated}) => any;
+  onGridRowsUpdated: ({ fromRow, toRow, updated }) => any;
 }
 
-interface IState {
-  rows: Array<object>;
-  columns: Array<object>;
-}
-
-class Grid extends React.Component<IProps, IState> {
-  public state: IState = {
-    rows: this.props.selectedRows,
-    columns: columns,
-  };
-
-  render() {
-    const { selectedRows, onGridRowsUpdated } = this.props;
-    return (
-      <Paper>
-        <ReactDataGrid
-          columns={columns}
-          rowGetter={i => selectedRows[i]}
-          rowsCount={selectedRows.length}
-          onGridRowsUpdated={onGridRowsUpdated}
-          enableCellSelect={true}
-        />
-        <Button onClick={() => {}}>Submit</Button>
-      </Paper>
-    );
-  }
-}
+const Grid: React.FC<IProps> = ({
+  columns,
+  selectedRows,
+  onGridRowsUpdated
+}) => {
+  return (
+    <Paper>
+      <ReactDataGrid
+        columns={columns}
+        rowGetter={i => selectedRows[i]}
+        rowsCount={selectedRows.length}
+        onGridRowsUpdated={onGridRowsUpdated}
+        enableCellSelect={true}
+      />
+      <Button onClick={() => {}}>Submit</Button>
+    </Paper>
+  );
+};
 
 export default Grid;

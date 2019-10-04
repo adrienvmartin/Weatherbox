d1756695351920796f7a09a0bfe1e1316b17d2d7
Adrien Martin <adrien.v.martin@gmail.com>
2019-10-04 10:20:31 -0600

    Clean up dataParser

--- src/dataParser.ts
+++ src/dataParser.ts
@@ -21,12 +21,6 @@ export const dataParser = state => {
     units.snow = "inch";
   }
 
-  // const dataRow = `|${month} ${field} ${unit} = ${input}`;
-
-  const generateRow = (month, field, unit) => {
-    // const specUnit = precipitation ? : units.precip (and so on)
-  };
-
   const months = columns.map(c => `|${c.name}`);
   const fields = state.data.map(r => ` ${r.field}`);
   const jan = state.data.map(v => v["jan"]);
@@ -60,55 +54,6 @@ export const dataParser = state => {
     .filter(d => d.selected === true)
     .map(d => d.templateCode);
 
-  const finalArray = [[months], [selectedRows]]; // finalArray.forEach => index 0 for months, index 1 for rows for selectedrows length, write out the month each time
-
-  const dataObj = {};
-
-  months.forEach(m => {
-    for (let i = 1; i < selectedRows.length; i++) {
-      // dataObj = { [`key${i}`]: m, [`field${i}`]: selectedRows[i] }
-      dataObj[`key${i}`] = m;
-      dataObj[`field${i}`] = selectedRows[i];
-    }
-    // dataObj[m] = selectedRows;
-  });
-
-  const display = [];
-  // @ts-ignore
-  const monthList = Object.values(dataObj).filter(o => o % 2);
-  months.forEach(m => {
-    for (let i = 0; i < months.length; i += 2) {
-      // @ts-ignore
-      display.push(m);
-    }
-  });
-
-  const rowObj = [];
-  const monthsObject = [];
-  selectedRows.forEach(row => {
-    for (let i = 0; i < selectedRows.length; i++) {
-      // @ts-ignore
-      monthsObject.push(months.forEach(m => m));
-    }
-  });
-
-  // months.forEach(m =>
-  // console.log(`${m} ${selectedRows.forEach(s => s)} ${units.temp}`)
-  // );
-
-  const returnMonth = (month, rows) => {
-    const monthHeader = month.repeat(rows.length);
-    const monthHeaderType = typeof monthHeader;
-    const rowHeader = rows.forEach(r => {
-      return r.toString();
-    });
-    // const combined = monthHeader.map((m, i) => { return `${m} ${rows[i]}`});
-    // console.log('monthHeader: ' + monthHeader + '\n rowHeader: ' + rows + '\n combined: ');
-    console.log(`rows: \n${rows}`);
-    console.log(`months: \n${months}`);
-    return `${monthHeader} ${rowHeader}`;
-  };
-
   const returnRowObj = () => {
     const looper = [];
     selectedRows.forEach(r => {
@@ -125,24 +70,5 @@ export const dataParser = state => {
     return looper.join("");
   };
 
-  // const test = months.forEach(m => returnMonth(m, selectedRows));
-
-  console.log(
-    "Months: \n" +
-      months +
-      "\n \n \n" +
-      "selectedRows: \n" +
-      selectedRows +
-      `
- \n \n \n` +
-      "dataObj: \n" +
-      Object.entries(dataObj) +
-      `\n \n \n`
-  );
-
-  // return `${header} \n ${collapsedSettings} \n ${openSettings} \n ${months} \n`;
-  // return `${Object.values(final[1])} ${Object.keys(final[1])} ${units.temp} = `;
-  // const datakeys = Object.entries(dataObj);
-  // datakeys.forEach(d => console.log(d));
   return `${header} \n ${location}${collapsedSettings}${openSettings}${singleLineSettings} \n \n ${returnRowObj()}${footer}`;
 };

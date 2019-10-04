9a55f541e5b8f095fe6d9cec71992447a0216a48
Adrien Martin <adrien.v.martin@gmail.com>
2019-10-04 12:56:30 -0600

    Remove Provider and Store from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
 import React, { Fragment } from "react";
-import { Provider } from "react-redux";
-import store from "./store";
 import "./App.css";
 import { CssBaseline, Container } from "@material-ui/core";
-import Main from "../src/components/Main";
+import Main from "./components/Main";
 
 function App() {
   return (
-    <Provider store={store}>
-      <Fragment>
-        <CssBaseline />
-        <Container>
-          <Main />
-        </Container>
-      </Fragment>
-    </Provider>
+    <Fragment>
+      <CssBaseline />
+      <Container>
+        <Main />
+      </Container>
+    </Fragment>
   );
 }
 

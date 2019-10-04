d247907e1b157a6f3611ecf7ad25a30c4884b431
Adrien Martin <adrien.v.martin@gmail.com>
2019-10-04 11:54:08 -0600

    Add colour settings

--- src/dataParser.ts
+++ src/dataParser.ts
@@ -1,64 +1,57 @@
-import { columns, Category } from "./constants";
+import { columns, Category, Colours } from "./constants";
 
 const { TEMP, PRECIP, SNOW } = Category;
 
+const { NONE, GREEN, BLUE, PASTEL, STANDARD } = Colours;
+
 export const dataParser = state => {
+  const {
+    metric,
+    collapsed,
+    open,
+    singleLine,
+    precipColour,
+    rainColour,
+    snowColour,
+    tempColour,
+    humidColour
+  } = state.settings;
+
   const header = "{{Weather box";
   const footer = "}}";
   const location = "|location = \n";
 
-  const collapsedSettings =
-    state.settings.collapsed === "true" ? `|collapsed = yes \n` : "";
-  const openSettings = state.settings.open === "true" ? `|open = yes \n` : "";
+  const collapsedSettings = collapsed === "true" ? `|collapsed = yes \n` : "";
+  const openSettings = open === "true" ? `|open = yes \n` : "";
   const singleLineSettings =
-    state.settings.singleLine === "true" ? `|singleLine = yes` : "";
+    singleLine === "true" ? `|singleLine = yes \n` : "";
 
-  const metricTemp = state.settings.metric === "true" ? "C" : "F";
-  const metricPrecip = state.settings.metric === "true" ? "mm" : "inch";
-  const metricSnow = state.settings.metric === "true" ? "cm" : "inch";
+  // Add precipitation days, colours here
+
+  const precipStatement = precipColour === NONE ? '' : `|precipitation colour = ${precipColour}\n`;
+  const rainStatement = rainColour === NONE ? '' : `|rain colour = ${rainColour}\n`;
+  const snowStatement = snowColour === NONE ? '' : `|snow colour = ${snowColour}\n`;
+  const tempStatement = tempColour === NONE ? '' : `|temperature colour = ${tempColour}\n`;
+  const humidStatement = humidColour === NONE ? '' : `|humidity colour = ${humidColour}\n`;
+
+  const metricTemp = metric === "true" ? "C" : "F";
+  const metricPrecip = metric === "true" ? "mm" : "inch";
+  const metricSnow = metric === "true" ? "cm" : "inch";
 
   const unitType = unit => {
-    if (unit.category === TEMP ) {
+    if (unit.category === TEMP) {
       return metricTemp;
     } else if (unit.category === PRECIP) {
       return metricPrecip;
     } else if (unit.category === SNOW) {
       return metricSnow;
     } else {
-      return '';
+      return "";
     }
   };
 
-  const jan = state.data.map(v => v["jan"]);
-  const feb = state.data.map(v => v["feb"]);
-  const mar = state.data.map(v => v["mar"]);
-  const apr = state.data.map(v => v["apr"]);
-  const may = state.data.map(v => v["may"]);
-  const jun = state.data.map(v => v["jun"]);
-  const jul = state.data.map(v => v["jul"]);
-  const aug = state.data.map(v => v["aug"]);
-  const sep = state.data.map(v => v["sep"]);
-  const oct = state.data.map(v => v["oct"]);
-  const nov = state.data.map(v => v["nov"]);
-  const dec = state.data.map(v => v["dec"]);
-  const monthState = {
-    jan,
-    feb,
-    mar,
-    apr,
-    may,
-    jun,
-    jul,
-    aug,
-    sep,
-    oct,
-    nov,
-    dec
-  };
-
-  const selectedRows = state.data
-    .filter(d => d.selected === true);
-    //.map(d => d.templateCode);
+  const selectedRows = state.data.filter(d => d.selected === true);
+  //.map(d => d.templateCode);
 
   const returnRowObj = () => {
     const looper = [];
@@ -77,7 +70,6 @@ export const dataParser = state => {
     });
     return looper.join("");
   };
-  console.log(monthState.jan);
 
-  return `${header} \n${location}${collapsedSettings}${openSettings}${singleLineSettings} \n \n${returnRowObj()}${footer}`;
+  return `${header} \n${location}${collapsedSettings}${openSettings}${singleLineSettings}${tempStatement}${precipStatement}${rainStatement}${snowStatement}${humidStatement} \n \n${returnRowObj()}${footer}`;
 };

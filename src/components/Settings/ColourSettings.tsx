7c4a97739d75caad663f14d99f3ab2d99ba20934
Adrien Martin <adrien.v.martin@gmail.com>
2019-10-04 12:41:37 -0600

    Improve code formatting

--- src/components/Settings/ColourSettings.tsx
+++ src/components/Settings/ColourSettings.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { Colours } from "../../constants";
 import {
   Radio,
@@ -28,83 +28,93 @@ const ColourSettings: React.FC<IProps> = ({
   const { NONE, BLUE, GREEN, PASTEL, STANDARD } = Colours;
 
   return (
-    <Fragment>
+    <div>
       <h1>Colours</h1>
       <p>
         Select the colors you want to use for temperature, precipitation, rain,
         and snow.
       </p>
-      <FormControl>
-        <FormLabel>Temperature</FormLabel>
-        <RadioGroup
-          aria-label="Temperature Colour"
-          name="tempColour"
-          value={tempColour}
-          onChange={onChange}
-        >
-          <FormControlLabel
-            value={STANDARD}
-            control={<Radio />}
-            label="Standard"
-          />
-          <FormControlLabel value={PASTEL} control={<Radio />} label="Pastel" />
-          <FormControlLabel value={NONE} control={<Radio />} label="None" />
-        </RadioGroup>
-      </FormControl>
-      <FormControl>
-        <FormLabel>Precipitation</FormLabel>
-        <RadioGroup
-          aria-label="Precipitation Colour"
-          name="precipColour"
-          value={precipColour}
-          onChange={onChange}
-        >
-          <FormControlLabel value={BLUE} control={<Radio />} label="Blue" />
-          <FormControlLabel value={GREEN} control={<Radio />} label="Green" />
-          <FormControlLabel value={NONE} control={<Radio />} label="None" />
-        </RadioGroup>
-      </FormControl>
-      <br />
-      <FormControl>
-        <FormLabel>Rain</FormLabel>
-        <RadioGroup
-          aria-label="Rain Colour"
-          name="rainColour"
-          value={rainColour}
-          onChange={onChange}
-        >
-          <FormControlLabel value={BLUE} control={<Radio />} label="Blue" />
-          <FormControlLabel value={GREEN} control={<Radio />} label="Green" />
-          <FormControlLabel value={NONE} control={<Radio />} label="None" />
-        </RadioGroup>
-      </FormControl>
-      <FormControl>
-        <FormLabel>Snow</FormLabel>
-        <RadioGroup
-          aria-label="Snow Colour"
-          name="snowColour"
-          value={snowColour}
-          onChange={onChange}
-        >
-          <FormControlLabel value={BLUE} control={<Radio />} label="Blue" />
-          <FormControlLabel value={GREEN} control={<Radio />} label="Green" />
-          <FormControlLabel value={NONE} control={<Radio />} label="None" />
-        </RadioGroup>
-      </FormControl>
-      <FormControl>
-        <FormLabel>Humidity</FormLabel>
-        <RadioGroup
-          aria-label="Humidity Colour"
-          name="humidColour"
-          value={humidColour}
-          onChange={onChange}
-        >
-          <FormControlLabel value={GREEN} control={<Radio />} label="Green" />
-          <FormControlLabel value={PASTEL} control={<Radio />} label="Pastel" />
-          <FormControlLabel value={NONE} control={<Radio />} label="None" />
-        </RadioGroup>
-      </FormControl>
-    </Fragment>
+      <div className="settingsContainer">
+        <FormControl>
+          <FormLabel>Temperature</FormLabel>
+          <RadioGroup
+            aria-label="Temperature Colour"
+            name="tempColour"
+            value={tempColour}
+            onChange={onChange}
+          >
+            <FormControlLabel
+              value={STANDARD}
+              control={<Radio />}
+              label="Standard"
+            />
+            <FormControlLabel
+              value={PASTEL}
+              control={<Radio />}
+              label="Pastel"
+            />
+            <FormControlLabel value={NONE} control={<Radio />} label="None" />
+          </RadioGroup>
+        </FormControl>
+        <FormControl>
+          <FormLabel>Precipitation</FormLabel>
+          <RadioGroup
+            aria-label="Precipitation Colour"
+            name="precipColour"
+            value={precipColour}
+            onChange={onChange}
+          >
+            <FormControlLabel value={BLUE} control={<Radio />} label="Blue" />
+            <FormControlLabel value={GREEN} control={<Radio />} label="Green" />
+            <FormControlLabel value={NONE} control={<Radio />} label="None" />
+          </RadioGroup>
+        </FormControl>
+        <br />
+        <FormControl>
+          <FormLabel>Rain</FormLabel>
+          <RadioGroup
+            aria-label="Rain Colour"
+            name="rainColour"
+            value={rainColour}
+            onChange={onChange}
+          >
+            <FormControlLabel value={BLUE} control={<Radio />} label="Blue" />
+            <FormControlLabel value={GREEN} control={<Radio />} label="Green" />
+            <FormControlLabel value={NONE} control={<Radio />} label="None" />
+          </RadioGroup>
+        </FormControl>
+        <FormControl>
+          <FormLabel>Snow</FormLabel>
+          <RadioGroup
+            aria-label="Snow Colour"
+            name="snowColour"
+            value={snowColour}
+            onChange={onChange}
+          >
+            <FormControlLabel value={BLUE} control={<Radio />} label="Blue" />
+            <FormControlLabel value={GREEN} control={<Radio />} label="Green" />
+            <FormControlLabel value={NONE} control={<Radio />} label="None" />
+          </RadioGroup>
+        </FormControl>
+        <FormControl>
+          <FormLabel>Humidity</FormLabel>
+          <RadioGroup
+            aria-label="Humidity Colour"
+            name="humidColour"
+            value={humidColour}
+            onChange={onChange}
+          >
+            <FormControlLabel value={GREEN} control={<Radio />} label="Green" />
+            <FormControlLabel
+              value={PASTEL}
+              control={<Radio />}
+              label="Pastel"
+            />
+            <FormControlLabel value={NONE} control={<Radio />} label="None" />
+          </RadioGroup>
+        </FormControl>
+      </div>
+    </div>
   );
 };
 

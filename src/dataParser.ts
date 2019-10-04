292cfce1f6d1c0323c87e5a916cf16352259f3c4
Adrien Martin <adrien.v.martin@gmail.com>
2019-10-04 11:32:24 -0600

    Fix metric unit functionality

--- src/dataParser.ts
+++ src/dataParser.ts
@@ -1,28 +1,34 @@
-import { columns } from "./constants";
+import { columns, Category } from "./constants";
+
+const { TEMP, PRECIP, SNOW } = Category;
 
 export const dataParser = state => {
   const header = "{{Weather box";
   const footer = "}}";
   const location = "|location = \n";
+
   const collapsedSettings =
     state.settings.collapsed === "true" ? `|collapsed = yes \n` : "";
   const openSettings = state.settings.open === "true" ? `|open = yes \n` : "";
   const singleLineSettings =
     state.settings.singleLine === "true" ? `|singleLine = yes` : "";
 
-  const units = { temp: "", precip: "", snow: "" };
-  if (state.settings.metric == "true") {
-    units.temp = "C";
-    units.precip = "mm";
-    units.snow = "cm";
-  } else {
-    units.temp = "F";
-    units.precip = "inch";
-    units.snow = "inch";
-  }
+  const metricTemp = state.settings.metric === "true" ? "C" : "F";
+  const metricPrecip = state.settings.metric === "true" ? "mm" : "inch";
+  const metricSnow = state.settings.metric === "true" ? "cm" : "inch";
 
-  const months = columns.map(c => `|${c.name}`);
-  const fields = state.data.map(r => ` ${r.field}`);
+  const unitType = unit => {
+    if (unit.category === TEMP ) {
+      return metricTemp;
+    } else if (unit.category === PRECIP) {
+      return metricPrecip;
+    } else if (unit.category === SNOW) {
+      return metricSnow;
+    } else {
+      return '';
+    }
+  };
+
   const jan = state.data.map(v => v["jan"]);
   const feb = state.data.map(v => v["feb"]);
   const mar = state.data.map(v => v["mar"]);
@@ -51,16 +57,18 @@ export const dataParser = state => {
   };
 
   const selectedRows = state.data
-    .filter(d => d.selected === true)
-    .map(d => d.templateCode);
+    .filter(d => d.selected === true);
+    //.map(d => d.templateCode);
 
   const returnRowObj = () => {
     const looper = [];
     selectedRows.forEach(r => {
       for (let i = 1; i < columns.length - 1; i++) {
         const col = columns[i].name;
+        const colId = columns[i].key;
         //@ts-ignore
-        looper.push(`|${col} ${r} = \n`);
+        looper.push(`|${col} ${r.templateCode} ${unitType(r)} = \n`);
+        // if (precip or snow is present in row) { looper.push(`|${col} ${r} {metricTemp} = \n`); }
         if (i % 12 === 0) {
           //@ts-ignore
           looper.push("\n");
@@ -69,6 +77,7 @@ export const dataParser = state => {
     });
     return looper.join("");
   };
+  console.log(monthState.jan);
 
-  return `${header} \n ${location}${collapsedSettings}${openSettings}${singleLineSettings} \n \n ${returnRowObj()}${footer}`;
+  return `${header} \n${location}${collapsedSettings}${openSettings}${singleLineSettings} \n \n${returnRowObj()}${footer}`;
 };
